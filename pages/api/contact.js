1b7d0a33d6d3731c8fa729d848045a5796a35f5a
Johannes <office@atelierbuchner.at>
2023-01-27 17:59:12 +0100

    event og

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -26,7 +26,7 @@ export default async (req, res) => {
                 html: `<p><strong>Name:</strong> ${name}</p> <p><strong>Email:</strong> ${email}</p> <p><strong>Telefon:</strong> ${phone}</p> <p><strong>Nachricht:</strong> ${message}</p>`,
             });
 
-            console.log("Message Sent", process.env.NEXT_USER);
+            console.log("Message Sent", process.env.NEXT_USER, email);
             res.status(200).json(req.body);
         } catch (err) {
             console.log("GEHT NET", err);
